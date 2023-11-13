aac4a99dea1d24fa9224cddeeeb785b1266c336b
ruicampos2 <ruigabriel2@ua.pt>
2023-11-13 14:52:35 +0000

    1 done

---
+++ programs/build/CMakeFiles/program1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/ruicampos/Desktop/IC/IC_Project2/opencv-bin/program1"
+  "/home/ruicampos/Desktop/IC/IC_Project2/opencv-bin/program1.pdb"
+  "CMakeFiles/program1.dir/program1.cpp.o"
+  "CMakeFiles/program1.dir/program1.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
